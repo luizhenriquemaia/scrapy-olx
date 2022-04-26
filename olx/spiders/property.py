b7dfd3c96896ab7546bd86e447576d9277e77e16
Luiz Henrique <luizhenriquembh@gmail.com>
2022-04-25 22:18:29 -0300

    fix olx changed page structure

--- olx/spiders/property.py
+++ olx/spiders/property.py
@@ -31,16 +31,41 @@ class QuotesSpider(scrapy.Spider):
 
     def parse_page(self, response):
         items = OlxItem()
-        property_list = response.css(".sc-1fcmfeb-2.fvbmlV > a")
+        property_list = response.css(".sc-1fcmfeb-2")
         for property_item in property_list:
-            content = property_item.css("div > div.fnmrjs-2.jiSLYe > div.fnmrjs-4.jflPgo")
             items['href'] = property_item.css("a::attr(href)").extract()
-            items['title'] = content.css("div.fnmrjs-6.iNpuEh>div>h2::attr(title)").extract()
-            items['text'] = content.css("div.fnmrjs-6.iNpuEh>div>span::text").extract()
-            items['price'] = content.css("div.fnmrjs-7.erUydy > div.fnmrjs-9.gqfQzY > div > div > span::text").extract()
-            if len(items['price']) > 1:
-                items['price'] = items['price'][0]
-            date_hour = content.css("div.fnmrjs-7.erUydy > div.fnmrjs-10.gHqbSa > div > div > span::text").extract()
-            items['date'] = date_hour[0]
-            items['hour'] = date_hour[1]
-            yield items  
+            content = property_item.css(".fnmrjs-2.jiSLYe")
+            if content == []:
+                print("\n2\n")
+                content = property_item.css("div.sc-12rk7z2-3.fqDYpJ")
+                items['title'] = content.css("div.sc-12rk7z2-5.fXzBqN > div >h2::attr(title)").extract()
+                items['text'] = content.css("div.sc-12rk7z2-6.bmfccv > div > div > span::text").extract()
+                items['price'] = content.css("div.sc-1kn4z61-1.hzqyCO > span::text").extract()
+                if len(items['price']) > 1:
+                    items['price'] = items['price'][0]
+                date_hour = content.css(".sc-11h4wdr-0.cHSTFT.sc-ifAKCX.cmFKIN::text").extract()
+                try:
+                    items['date'] = date_hour[0]
+                except IndexError:
+                    items['date'] = ""
+                try:
+                    items['hour'] = date_hour[1]
+                except IndexError:
+                    items['hour'] = ""
+            else:
+                print("\n1\n")
+                items['title'] = content.css(".fnmrjs-6.iNpuEh>div>h2::attr(title)").extract()
+                items['text'] = content.css("div.fnmrjs-6.iNpuEh>div>span::text").extract()
+                items['price'] = content.css("div.fnmrjs-7.erUydy > div.fnmrjs-9.gqfQzY > div > div > span::text").extract()
+                if len(items['price']) > 1:
+                    items['price'] = items['price'][0]
+                date_hour = content.css("div.fnmrjs-7.erUydy > div.fnmrjs-10.gHqbSa > div > div > span::text").extract()
+                try:
+                    items['date'] = date_hour[0]
+                except IndexError:
+                    items['date'] = ""
+                try:
+                    items['hour'] = date_hour[1]
+                except IndexError:
+                    items['hour'] = ""
+            yield items
